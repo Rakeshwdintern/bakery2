79c45aceea0b2c030875cf78d983271c76d28806
Rakeshwdintern <59331949+Rakeshwdintern@users.noreply.github.com>
2020-01-07 15:42:23 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,8 @@
+function slide() {
+    var visible = document.getElementById("sideNav");
+    visible.style.display = "block";
+
+}
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle('active');
+}
